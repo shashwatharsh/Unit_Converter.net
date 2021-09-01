a97bb42fd1646b1349cfc4154e1779ceeb591c47
Shashwat <harshshashwat18@gmail.com>
2021-09-01 22:11:39 +0530

    Updating length converter

--- Unit_converter/Form1.Designer.cs
+++ Unit_converter/Form1.Designer.cs
@@ -35,42 +35,46 @@ namespace Unit_converter
             this.M = new System.Windows.Forms.Label();
             this.cm = new System.Windows.Forms.Label();
             this.clength = new System.Windows.Forms.Button();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(246, 33);
+            this.label1.Font = new System.Drawing.Font("Segoe UI Emoji", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(255, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(293, 39);
+            this.label1.Size = new System.Drawing.Size(299, 47);
             this.label1.TabIndex = 0;
             this.label1.Text = "Length converter";
             // 
             // rtb1
             // 
             this.rtb1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.rtb1.Location = new System.Drawing.Point(183, 157);
+            this.rtb1.Location = new System.Drawing.Point(78, 106);
             this.rtb1.Name = "rtb1";
-            this.rtb1.Size = new System.Drawing.Size(100, 29);
+            this.rtb1.Size = new System.Drawing.Size(216, 29);
             this.rtb1.TabIndex = 1;
             this.rtb1.TextChanged += new System.EventHandler(this.rtb1_TextChanged);
             // 
             // rtb2
             // 
+            this.rtb2.BackColor = System.Drawing.SystemColors.Control;
             this.rtb2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.rtb2.Location = new System.Drawing.Point(489, 158);
+            this.rtb2.Location = new System.Drawing.Point(535, 103);
             this.rtb2.Name = "rtb2";
             this.rtb2.ReadOnly = true;
-            this.rtb2.Size = new System.Drawing.Size(100, 29);
+            this.rtb2.Size = new System.Drawing.Size(216, 29);
             this.rtb2.TabIndex = 2;
             // 
             // M
             // 
             this.M.AutoSize = true;
             this.M.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.M.Location = new System.Drawing.Point(290, 161);
+            this.M.Location = new System.Drawing.Point(300, 108);
             this.M.Name = "M";
             this.M.Size = new System.Drawing.Size(26, 24);
             this.M.TabIndex = 3;
@@ -80,7 +84,7 @@ namespace Unit_converter
             // 
             this.cm.AutoSize = true;
             this.cm.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.cm.Location = new System.Drawing.Point(595, 161);
+            this.cm.Location = new System.Drawing.Point(757, 106);
             this.cm.Name = "cm";
             this.cm.Size = new System.Drawing.Size(39, 24);
             this.cm.TabIndex = 4;
@@ -89,7 +93,7 @@ namespace Unit_converter
             // clength
             // 
             this.clength.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.clength.Location = new System.Drawing.Point(316, 235);
+            this.clength.Location = new System.Drawing.Point(344, 287);
             this.clength.Name = "clength";
             this.clength.Size = new System.Drawing.Size(150, 54);
             this.clength.TabIndex = 5;
@@ -97,20 +101,77 @@ namespace Unit_converter
             this.clength.UseVisualStyleBackColor = true;
             this.clength.Click += new System.EventHandler(this.clength_Click);
             // 
-            // comboBox1
+            // listBox1
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(183, 204);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 21);
-            this.comboBox1.TabIndex = 6;
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 18;
+            this.listBox1.Items.AddRange(new object[] {
+            "Meter",
+            "Kilometer",
+            "Centimeter",
+            "Micrometer",
+            "Nanometer",
+            "Mile",
+            "Yard",
+            "Foot",
+            "Inch",
+            "Lightyear"});
+            this.listBox1.Location = new System.Drawing.Point(78, 177);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(216, 130);
+            this.listBox1.TabIndex = 6;
+            // 
+            // listBox2
+            // 
+            this.listBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 18;
+            this.listBox2.Items.AddRange(new object[] {
+            "Meter",
+            "Kilometer",
+            "Centimeter",
+            "Micrometer",
+            "Nanometer",
+            "Mile",
+            "Yard",
+            "Foot",
+            "Inch",
+            "Lightyear"});
+            this.listBox2.Location = new System.Drawing.Point(535, 177);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(216, 130);
+            this.listBox2.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Historic", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(21, 106);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 20);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "From:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI Historic", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(479, 106);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 20);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "To:";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.comboBox1);
+            this.ClientSize = new System.Drawing.Size(897, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
             this.Controls.Add(this.clength);
             this.Controls.Add(this.cm);
             this.Controls.Add(this.M);
@@ -132,7 +193,10 @@ namespace Unit_converter
         private System.Windows.Forms.Label M;
         private System.Windows.Forms.Label cm;
         private System.Windows.Forms.Button clength;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- Unit_converter/Form1.cs
+++ Unit_converter/Form1.cs
@@ -15,6 +15,8 @@ namespace Unit_converter
         public Form1()
         {
             InitializeComponent();
+            listBox1.SelectedItem = "Meter";
+            listBox2.SelectedItem = "Kilometer";
         }
 
         private void rtb1_TextChanged(object sender, EventArgs e)
@@ -24,8 +26,23 @@ namespace Unit_converter
 
         private void clength_Click(object sender, EventArgs e)
         {
-            double value2 = double.Parse(rtb1.Text);
-            rtb2.Text = (value2 * 100).ToString();
+            if (listBox1.SelectedItem == listBox2.SelectedItem)
+            {
+                rtb2.Text = double.Parse(rtb1.Text).ToString();
+            }
+            if (listBox1.SelectedItem == "Meter" &&listBox2.SelectedItem =="Kilometer")
+            {
+                rtb2.Text = (double.Parse(rtb1.Text)*0.001).ToString();
+            } if (listBox1.SelectedItem == "Meter" &&listBox2.SelectedItem =="Centimeter")
+            {
+                rtb2.Text = (double.Parse(rtb1.Text)*100).ToString();
+            } if (listBox1.SelectedItem == "Kilometer" &&listBox2.SelectedItem =="Meter")
+            {
+                rtb2.Text = (double.Parse(rtb1.Text)*1000).ToString();
+            }
+
+           // double value2 = double.Parse(rtb1.Text);
+           // rtb2.Text = (value2 * 100).ToString();
         }
     }
 }
